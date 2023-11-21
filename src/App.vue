fd54e43bda2643bda115333d72b35b063a7b258a
wushijie <wushijie@melot.cn>
2023-11-21 11:00:37 +0800

    优化nav展示逻辑

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-// import { ref } from 'vue';
-import { useRoute } from 'vue-router';
 import Nav from '~/components/nav.vue'
 
-const route = useRoute()
 // 后台不需要显示Nav
-const isNavVisible = route.path.includes('activity_')
+const isNavVisible = location.pathname.includes('activity_')
 
 </script>
 
