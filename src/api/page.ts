e03b577e7a4ad992a615a53d089e05c6f94ddc67
wushijie <wushijie@melot.cn>
2023-11-29 19:21:51 +0800

    add apis

---
+++ src/api/page.ts
@@ -0,0 +1,8 @@
+import { get, post } from "./request";
+
+// 登录
+export const loginAPI = (userId: number, password: string) => post('/v3/login', { userId, password })
+// 退出登录
+export const logoutAPI = (userId: number) => post('/v3/logout', { userId })
+// 获取profile
+export const profileInfoAPI = () => get('/v3/profileInfo')
--- src/store/useLoginStore.ts
+++ src/store/useLoginStore.ts
@@ -1,6 +1,32 @@
 import { defineStore } from 'pinia'
 
+export interface ProfileConfig {
+    userId: number
+    username: string
+    nickName: string
+    avatar: string
+    password: string
+    money: number
+    coupon: number
+    gender: number
+    identity: number
+    userLevel: number
+    actorLevel: number
+    talent: number
+    familyId: number
+    familyName: string
+    birthday: string
+    genderName: string
+    identityName: string
+    talentName: string
+    createDate: number
+    isActor: boolean
+    isLogin: boolean
+    lastLoginDate: number
+    token: string
+}
+
 export const useLoginStore = defineStore('login', {
     state: () => ({ profile: {} }),
-    actions: () => {},
+    actions: {},
 })
