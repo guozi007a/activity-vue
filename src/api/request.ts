2c4c99ba5130251c4036515ab9575a004958ef75
wushijie <wushijie@melot.cn>
2023-12-12 10:15:57 +0800

    修改请求封装，优化请求拦截

--- src/api/request.ts
+++ src/api/request.ts
@@ -7,26 +7,41 @@ const instance = axios.create({
 
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
-    console.log('config data: ', config.data, typeof config.data)
-    console.log('parse data: ', config.data && JSON.parse(config.data))
-    if (config.data) {
-        parseData = JSON.parse(config.data)
-        if (parseData.userId && parseData.token) {
-            config.headers.userId = parseData.userId
-            config.headers.token = parseData.token
+    if (config.params?.userId && config.params?.token) { // 如果参数对象中同时存在userId&token，就将它们放入请求头
+        config.headers.userId = config.params.userId
+        config.headers.token = config.params.token
+        delete config.params.userId
+        delete config.params.token
+    } else if (config.data) {
+        if (config.data?.userId && config.data?.token) { // 如果参数对象中同时存在userId&token，就将它们放入请求头
+            config.headers.userId = config.data.userId
+            config.headers.token = config.data.token
+            delete config.data.userId
+            delete config.data.token
         }
+
+        // 在post请求中，参数是需要转化的，不能直接传对象。
+        // 为了能将userId & token放到请求头，那么在放入前，就不能先转化参数，不然config.data就不是原来的对象了，而是转化后的字符串
+        // 所以转化参数就要放到放入请求头后来处理
+        // 为了便于根据不同类型来转化不同的data形式，所以在headers中携带了type字段
+        if (config.headers?.type === 'json') {
+            config.data = JSON.stringify(config.data)
+            config.headers['Content-Type'] = "application/json; charset=UTF-8"
+        } else if (config.headers?.type === 'form') {
+            let result = ''
+            for (const [k, v] of Object.entries(config.data)) {
+                if (v === null || v === undefined || v === '') {
+                    continue
+                }
+                result = result
+                    ? (result + `&${k}=${encodeURIComponent(v as any)}`)
+                    : `${k}=${encodeURIComponent(v as any)}`
+            }
+            config.data = result
+            config.headers['Content-Type'] = "application/x-www-form-urlencoded; charset=UTF-8"
+        }
+        delete config.headers.type
     }
-    // if (config.params?.userId && config.params?.token) {
-    //     config.headers.userId = config.params.userId
-    //     config.headers.token = config.params.token
-    //     delete config.params.userId
-    //     delete config.params.token
-    // } else if (config.data?.userId && config.data?.token) {
-    //     config.headers.userId = config.data.userId
-    //     config.headers.token = config.data.token
-    //     delete config.data.userId
-    //     delete config.data.token
-    // }
     return config;
 }, function (error) {
     // 对请求错误做些什么
@@ -82,32 +97,21 @@ export const post = (url: string, params?: Record<string, any>): Promise<ResType
     return instance({
         url,
         method: 'post',
-        data: params ? JSON.stringify(params) : {},
+        data: params,
         headers: {
-            "Content-Type": "application/json; charset=UTF-8"
+            "type": "json",
         },
     })
 }
 
 // post form request
 export const postForm = (url: string, params?: Record<string, any>): Promise<ResType> => {
-    let result = ''
-    if (params) {
-        for (const [k, v] of Object.entries(params)) {
-            if (v === null || v === undefined || v === '') {
-                continue
-            }
-            result = result
-                ? (result + `&${k}=${encodeURIComponent(v)}`)
-                : `${k}=${encodeURIComponent(v)}`
-        }
-    }
     return instance({
         url,
         method: 'post',
-        data: result,
+        data: params,
         headers: {
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+            "type": "form",
         },
     })
 }
